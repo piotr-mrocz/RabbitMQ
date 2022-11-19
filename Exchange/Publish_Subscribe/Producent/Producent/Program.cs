acdc6e922ee6bca1de5ca8aaaa997554d0ed5ab2
Piotr Mroczkowski <p.mrocz@vp.pl>
2022-11-19 01:44:55 +0100

    Create Producent project

---
+++ Exchange/Publish_Subscribe/Producent/Producent/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.Title = "Producent";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare(
+            exchange: "PublishSubcribe",
+            type: ExchangeType.Fanout,
+            durable: true,
+            autoDelete: false,
+            arguments: null);
+
+        var isFinished = false;
+
+        do
+        {
+            Console.Write("Wpisz wiadomość: ");
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message))
+                continue;
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "PublishSubcribe",
+                routingKey: string.Empty,
+                basicProperties: null,
+                body: body);
+
+            Console.Write($"Wysłana wiadomość: {message}");
+            Console.WriteLine($"Wciśniej klawisz {ConsoleKey.Escape}, aby opuścić program");
+            Console.WriteLine();
+
+            var readKey = Console.ReadKey();
+
+            if (readKey.Key == ConsoleKey.Escape)
+                isFinished = true;
+
+        } while (!isFinished);
+    }
+}
---
+++ Exchange/Publish_Subscribe/Subscriber/Subscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Consumer tworzy kolejkę w chwili powstania. Dlatego wiadomości wysłane przez Producenta zanim
+// zostanie stworzona kolejka przepadają.
+
+Console.Title = "Consumer 1";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare(
+            exchange: "PublishSubcribe",
+            type: ExchangeType.Fanout,
+            durable: true,
+            autoDelete: false,
+            arguments: null);
+
+        var queueName = channel.QueueDeclare().QueueName;
+
+        channel.QueueBind(
+            queue: queueName,
+            exchange: "PublishSubcribe",
+            routingKey: string.Empty);
+
+        Console.WriteLine("Czekam na wiadomości...");
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Otrzymana wiadomość: {message}");
+        };
+
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+        Console.WriteLine("Wciśnij jakikolwiek klawisz aby zakończyć program");
+        Console.ReadKey();
+    }
+}
